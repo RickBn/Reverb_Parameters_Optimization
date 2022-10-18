8bd1f64b0385baaee7c6aa2b344264f273df554a
Riccardo Bona <bonariccardo9@gmail.com>
2022-10-18 12:20:02 +0200

    Completed generate_survey

--- scripts/statistics/generate_survey.py
+++ scripts/statistics/generate_survey.py
@@ -80,6 +80,7 @@ class SurveyGenerator:
 if __name__ == "__main__":
     subject_idx = 0
     survey_setup = json_load("scripts/statistics/survey_setup.json")
+    trial_setups = survey_setup["trial_setups"]
     conditions = [("complexity", "latin"),
                   ("room", "shuffle"),
                   ("conditions", "shuffle"),
@@ -93,36 +94,59 @@ if __name__ == "__main__":
             for condition, speakers in conditions.items():
                 conditions[condition] = speakers[:int(complexity)]
 
-workbook = xlsxwriter.Workbook('Survey.xlsx')
-worksheet = workbook.add_worksheet()
+    workbook = xlsxwriter.Workbook('Survey.xlsx')
+    worksheet = workbook.add_worksheet()
 
-cell_format = workbook.add_format({'border': 1, 'align': 'left'})
+    cell_format = workbook.add_format({'border': 1, 'align': 'left'})
+    impostor_format = workbook.add_format({'border': 1, 'align': 'left', 'bg_color': 'cyan'})
 
-worksheet.set_column(0, 7, 15)
+    worksheet.set_column(0, 7, 17)
 
-merge_format = workbook.add_format({
-    'bold': 1,
-    'border': 2,
-    'align': 'center',
-    'valign': 'vcenter'})
+    merge_format = workbook.add_format({
+        'bold': 1,
+        'border': 2,
+        'align': 'center',
+        'valign': 'vcenter'})
 
-for c_col, complexity in enumerate(randomized_dict.keys()):
-    c_idx = c_col * 2
-    worksheet.merge_range(0,
-                          c_idx,
-                          0,
-                          c_idx + 1,
-                          f'Complexity_{complexity}',
-                          merge_format)
-    for r_row, room in enumerate(randomized_dict[complexity].keys()):
-        r_idx = r_row * (int(complexity) + 2)
-        worksheet.merge_range(r_idx + 1,
+    for c_col, complexity in enumerate(randomized_dict.keys()):
+        c_idx = c_col * 2
+        comp = int(complexity)
+        worksheet.merge_range(0,
                               c_idx,
-                              r_idx + 1,
+                              0,
                               c_idx + 1,
-                              room,
+                              f'Complexity_{complexity}',
                               merge_format)
-        worksheet.write(r_idx + 2, c_idx, "same_room", cell_format)
-        worksheet.write_blank(r_idx + 2, c_idx + 1, None, cell_format)
+        for r_row, room in enumerate(randomized_dict[complexity].keys()):
+            conditions = randomized_dict[complexity][room].keys()
+            n_conditions = len(conditions)
+            r_idx = (r_row * ((comp + 2) * n_conditions)) + (r_row + 1)
+            worksheet.merge_range(r_idx,
+                                  c_idx,
+                                  r_idx,
+                                  c_idx + 1,
+                                  room,
+                                  merge_format)
 
-workbook.close()
+            for cond_idx, condition in enumerate(conditions):
+                cond_row = r_idx + (cond_idx * (comp + 2)) + 1
+                worksheet.merge_range(cond_row,
+                                      c_idx,
+                                      cond_row,
+                                      c_idx + 1,
+                                      condition,
+                                      merge_format)
+                worksheet.write(cond_row + 1, c_idx, "same_room", cell_format)
+                worksheet.write_blank(cond_row + 1, c_idx + 1, None, cell_format)
+
+                for i, speaker in enumerate(randomized_dict[complexity][room][condition]):
+                    impostor = trial_setups[room][complexity]['impostor'][condition]
+                    position = trial_setups[room][complexity]['position'][i]
+                    if impostor != position:
+                        pos_format = cell_format
+                    else:
+                        pos_format = impostor_format
+                    worksheet.write(cond_row + i + 2, c_idx, f'{position}-{speaker}', pos_format)
+                    worksheet.write_blank(cond_row + i + 2, c_idx + 1, None, cell_format)
+
+    workbook.close()
