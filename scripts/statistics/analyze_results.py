3da1423c47cf73f18620a715f145df7cbbf7bfc9
RickBn <bonariccardo9@gmail.com>
2022-11-16 15:52:33 +0100

    Update analyze_results

--- scripts/statistics/analyze_results.py
+++ scripts/statistics/analyze_results.py
@@ -3,26 +3,27 @@ import pandas as pd
 df = pd.read_excel("test_results/Subject_1_028.xlsx", engine='openpyxl', sheet_name=None)
 questionnaire = df.pop('Questionnaire')
 
-a = df[0].iloc[:, [0, 1]]
+for complexity in df.keys():
+    comp_df = df[complexity].iloc[:, [0, 1]]
 
-room = a.columns[0]
+    room = comp_df.columns[0]
 
-a = a.rename(columns={a.columns[1]: f'{room}_answer'})
+    comp_df = comp_df.rename(columns={comp_df.columns[1]: f'{room}_answer'})
 
-b = a.iloc[0:2+2, :]
+    b = comp_df.iloc[0:2+2, :]
 
-condition = b.iloc[0, 0]
-df_2 = pd.DataFrame(columns=['Condition', 'Answer', 'Speaker'])
+    condition = b.iloc[0, 0]
+    df_2 = pd.DataFrame(columns=['Condition', 'Answer', 'Speaker'])
 
-if condition == 'NONE':
-    if b.iloc[1, 1] == 'Yes':
-        df_2.loc[len(df_2.index)] = [condition, 1, 'None']
+    if condition == 'NONE':
+        if b.iloc[1, 1] == 'Yes':
+            df_2.loc[len(df_2.index)] = [condition, 1, 'None']
+        else:
+            speaker = b.iloc[2:, ].dropna().iloc[0, 0]
+            df_2.loc[len(df_2.index)] = [condition, 0, speaker]
+
     else:
-        speaker = b.iloc[2:, ].dropna().iloc[0, 0]
-        df_2.loc[len(df_2.index)] = [condition, 0, speaker]
-
-else:
-    row = b.iloc[2:, ].dropna()
-    df_2.loc[len(df_2.index)] = [condition, row.iloc[0, 1], row.iloc[0, 0]]
+        row = b.iloc[2:, ].dropna()
+        df_2.loc[len(df_2.index)] = [condition, row.iloc[0, 1], row.iloc[0, 0]]
 
 
